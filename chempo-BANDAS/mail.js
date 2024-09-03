ede2b3e32f6e912bc2014e7013a076f8e489de58
Facundo <=>
2024-09-02 21:51:49 -0300

    formulario

---
+++ chempo-BANDAS/mail.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('button');
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  btn.value = 'Enviando...';
+
+  const serviceID = 'service_u7j3g1k';
+  const templateID = 'template_7v7pnxg';
+
+  emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Enviado con exito!');
+      form.reset()
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+      form.reset()
+    });
+});
